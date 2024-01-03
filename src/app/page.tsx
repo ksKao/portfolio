fe87473dab600c50ad2232ef971f32d3be0e6049
Kao Kai Siang <kaokaisiang021001@gmail.com>
2024-01-03 12:44:45 +0800

    Fixed submit button and added placeholder footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Page() {
 	return (
@@ -14,6 +15,7 @@ export default function Page() {
 			<Skills />
 			<Projects />
 			<Contact />
+			<Footer />
 		</>
 	);
 }
