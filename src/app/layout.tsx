cffb4cfe957528ae524ff3af3c30c38af923da6d
Kao Kai Siang <kaokaisiang021001@gmail.com>
2024-01-08 12:22:41 +0800

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<body className={inter.className}>
 				<Toaster />
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	);
